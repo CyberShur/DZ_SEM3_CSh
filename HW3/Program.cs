b3a588c3a46d3aa7e7b5608a8818cd2c866b9a92
Aleksandr <aldrobakha@gmail.com>
2022-04-20 20:30:14 +0300

    add HW3

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -3,3 +3,24 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.Write("Введите число N: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+if (Number <= 0)
+{
+    Console.WriteLine("Число должно быть положительным и не равняться нулю");
+}
+
+while (i <= Number)
+{
+    double result = sqrNumber(i);
+    Console.Write(result + " ");
+    i++;
+}
+
+double sqrNumber(int Number)
+{
+    return Math.Pow(Number, 3);
+}

51528eb49be97416a71e820f289bf810f88c5e9b
Aleksandr <aldrobakha@gmail.com>
2022-04-23 21:55:23 +0300

    add HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -4,12 +4,41 @@
 // 23432 -> да
 // 12821 -> да
 
-Console.Write("Введите число: "); 
+Console.Write("Введите пятизначное число число: "); 
 int Input = Convert.ToInt32(Console.ReadLine());
 string array = Input.ToString();
-int[] newArray = new int[array.Length];
-int[] reversArray = new int[array.Length];
 
+bool Palindrome()
+{
+    bool Сompare = true;
+    for (int count = 0; count < array.Length / 2; count++)
+    {
+        if (array[count] != array[array.Length - 1 - count])
+        {
+            Сompare = false;
+        }
+    }
+    return Сompare;
+}
+
+if (Palindrome())
+{
+    Console.WriteLine($"Число {Input} является палиндромом");
+}
+else
+{
+    Console.WriteLine($"Число {Input} НЕ является палиндромом");
+}
+
+
+
+
+
+
+
+
+
+/* int[] reversArray = new int[array.Length];
 
 for(int count=0; count < array.Length; count++)
 {
@@ -30,7 +59,7 @@ if (Input == NumberRevers)
 else
 {
     Console.WriteLine($"Число {Input} НЕ является палиндромом");
-}
+}  */
 
 
 
